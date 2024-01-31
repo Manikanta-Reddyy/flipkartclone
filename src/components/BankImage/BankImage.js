dfba42184b62f6db467daa0cf9a68b28facbfc5e
Manikanta-Reddyy <94749162+Manikanta-Reddyy@users.noreply.github.com>
2024-01-31 09:50:20 +0530

    Update BankImage.js

--- src/components/BankImage/BankImage.js
+++ src/components/BankImage/BankImage.js
@@ -4,7 +4,7 @@ import "./BankImage.css";
 function BankImage() {
   return (
     <div className="bank_image">
-      <img src="https://images-eu.ssl-images-amazon.com/images/G/31/Events/JANART2023/BankStripe/Event_Bank_Stripe_PC.jpg" />
+      <img src="https://images-eu.ssl-images-amazon.com/images/G/31/Events/JANART2023/BankStripe/Event_Bank_Stripe_PC.jpg" alt="Description of the image"/>
     </div>
   );
 }
